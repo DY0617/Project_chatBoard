735171b06f88635ba731dcd08bf83f661aebaa7d
dongyeop <brassdong@gmail.com>
2023-03-03 23:53:10 +0900

    rest api comment

---
+++ src/main/java/com/study/domain/comment/CommentResponse.java
@@ -0,0 +1,19 @@
+package com.study.domain.comment;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CommentResponse {
+
+    private Long id;                       // 댓글 번호 (PK)
+    private Long postId;                   // 게시글 번호 (FK)
+    private String content;                // 내용
+    private String writer;                 // 작성자
+    private Boolean deleteYn;              // 삭제 여부
+    private LocalDateTime createdDate;     // 생성일시
+    private LocalDateTime modifiedDate;    // 최종 수정일시
+
+}
