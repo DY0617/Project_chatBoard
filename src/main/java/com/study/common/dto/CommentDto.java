70d53e89ea1ec1681df060b6d87d94f5449d27b3
dongyeop <brassdong@gmail.com>
2023-03-04 13:28:15 +0900

    comment

---
+++ src/main/java/com/study/common/dto/CommentDto.java
@@ -0,0 +1,17 @@
+package com.study.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class CommentDto {
+    private Long id;
+    private Long articleId;
+    private String nickname;
+    private String body;
+}
---
+++ src/main/java/com/study/domain/comment/CommentController.java
@@ -0,0 +1,16 @@
+package com.study.domain.comment;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommentController {
+
+
+}
