e362001fbd31c1b8c32cd7cbd7fedac571d2d0c8
dongyeop <brassdong@gmail.com>
2023-03-04 13:57:14 +0900

    comment

--- src/main/java/com/study/domain/comment/CommentController.java
+++ src/main/java/com/study/domain/comment/CommentController.java
@@ -1,16 +1,17 @@
 package com.study.domain.comment;
 
+import java.security.Principal;
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+
+@RequestMapping("/comment")
 @RestController
 public class CommentController {
-
 
 }
