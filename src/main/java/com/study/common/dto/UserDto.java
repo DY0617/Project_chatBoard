8bef33304ff57b8d840ff8f7b73fc8d2ae567379
dongyeop <brassdong@gmail.com>
2023-03-07 19:12:40 +0900

    로그인/회원가입 구현 완료

--- src/main/java/com/study/common/dto/UserDto.java
+++ src/main/java/com/study/common/dto/UserDto.java
@@ -19,11 +19,11 @@ public class UserDto {
 
         private Long id;
 
-        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{4,20}$", message = "아이디는 특수문자를 제외한 4~20자리여야 합니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "아이디는 특수문자를 제외한 영문과 숫자로 이루어진 4~20자리여야 합니다.")
         @NotBlank(message = "아이디는 필수 입력 값입니다.")
         private String username;
 
-        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[#?!@$%^&*-]).{8,16}$", message = "비밀번호는 영문, 숫자, 특수문자를 모두 포함한 8~16자리여야 합니다.")
         private String password;
 
         @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
