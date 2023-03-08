e636be27d1f2209d2fc381ce2bd70ae9154d30f2
dongyeop <brassdong@gmail.com>
2023-03-08 17:48:59 +0900

    댓글

---
+++ src/main/java/com/study/domain/comment/CommentRequest.java
@@ -0,0 +1,18 @@
+package com.study.domain.comment;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentRequest {
+
+    private Long id;           // 댓글 번호 (PK)
+    private String postId;     // 게시글 번호 (FK)
+    private String content;    // 내용
+    private String writer;     // 작성자
+    private Long userId;
+
+}
