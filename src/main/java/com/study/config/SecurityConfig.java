f518396da7f28a9a93f983e35ee87869bc3d6284
dongyeop <brassdong@gmail.com>
2023-04-02 17:55:33 +0900

    package

---
+++ src/main/java/com/study/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.study.config;
+
+
+import com.study.common.security.auth.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+/**
+ * Security 설정 클래스
+ */
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomUserDetailsService customUserDetailsService;
+  private final AuthenticationFailureHandler customFailureHandler;
+
+  @Bean
+  public BCryptPasswordEncoder encoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(customUserDetailsService).passwordEncoder(encoder());
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  
+          @Override
+  public void configure(WebSecurity web) throws Exception {
+    web
+        .ignoring().antMatchers( "/css/**", "/js/**", "/img/**");
+  }
+  
+          @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/post/list.do", "/auth/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/auth/login")
+        .loginProcessingUrl("/auth/loginProc")
+            .failureHandler(customFailureHandler)
+        .defaultSuccessUrl("/post/list.do")
+
+            .and()
+        .logout()
+        .logoutSuccessUrl("/post/list.do")
+        .invalidateHttpSession(true).deleteCookies("JSESSIONID");
+  }
+
+}
---
+++ src/main/java/com/study/config/WebMvcConfig.java
@@ -0,0 +1,33 @@
+package com.study.config;
+
+import com.study.common.security.auth.LoginUserArgumentResolver;
+import com.study.interceptor.LoggerInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor())
+                .excludePathPatterns("/css/**", "/images/**", "/js/**");
+    }
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+
+
+}
