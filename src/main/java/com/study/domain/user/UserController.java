6cb1b3e3a285bf6915ae3edb18d961562a308a6a
dongyeop <brassdong@gmail.com>
2023-04-05 00:17:41 +0900

    test

--- src/main/java/com/study/domain/user/UserController.java
+++ src/main/java/com/study/domain/user/UserController.java
@@ -58,6 +58,9 @@ public class UserController {
 
     @PostMapping("/auth/joinProc")
     public String joinProc(@Valid UserDto.Request dto, Errors errors, Model model) {
+
+        System.out.println("PostMapping JoinProc");
+
         if (errors.hasErrors()) {
             /* 회원가입 실패시 입력 데이터 값을 유지 */
             model.addAttribute("userDto", dto);
