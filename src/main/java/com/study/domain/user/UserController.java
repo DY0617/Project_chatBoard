079fa39585222aa878eb69e13801c7290fabff6b
dongyeop <brassdong@gmail.com>
2023-05-21 18:41:21 +0900

    Update UserController.java

--- src/main/java/com/study/domain/user/UserController.java
+++ src/main/java/com/study/domain/user/UserController.java
@@ -58,8 +58,7 @@ public class UserController {
 
     @PostMapping("/auth/joinProc")
     public String joinProc(@Valid UserDto.Request dto, Errors errors, Model model) {
-
-        System.out.println("PostMapping JoinProc");
+        
 
         if (errors.hasErrors()) {
             /* 회원가입 실패시 입력 데이터 값을 유지 */
