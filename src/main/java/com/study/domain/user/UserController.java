c662be516ce3035423adc9373bbe591c5cfe044b
dongyeop <brassdong@gmail.com>
2023-05-21 21:06:29 +0900

    Update UserController.java

--- src/main/java/com/study/domain/user/UserController.java
+++ src/main/java/com/study/domain/user/UserController.java
@@ -78,10 +78,12 @@ public class UserController {
 
 
     @GetMapping("/auth/login")
-    public String login(
+    public String login(@RequestParam(value = "error", required = false)String error,
+                        @RequestParam(value = "exception", required = false)String exception,
                         Model model) {
 
-
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
         return "/user/login";
     }
 
