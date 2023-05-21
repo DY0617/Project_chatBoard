fbb220d74ca5eadfe6c9e28a8659c059e4dd835f
dongyeop <brassdong@gmail.com>
2023-05-21 22:28:13 +0900

    csrf

--- src/test/java/com/study/PostServiceTest.java
+++ src/test/java/com/study/PostServiceTest.java
@@ -18,7 +18,7 @@ public class PostServiceTest {
         PostRequest params = new PostRequest();
         params.setTitle("1번 게시글 제목");
         params.setContent("1번 게시글 내용");
-        params.setWriter("테스터");
+        params.setWriter("관리자");
         params.setNoticeYn(false);
         Long id = postService.savePost(params);
         System.out.println("생성된 게시글 ID : " + id);
